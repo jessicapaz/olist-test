d9cb13e890ecc78733408c5c940a2149e8887989
Jéssica Paz <jessicamorim.42@gmail.com>
2018-07-01 15:19:18 -0300

    Add subscriber view

--- call_records/api/views.py
+++ call_records/api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Subscriber
+from .serializers import SubscriberSerializer
+
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.generics import CreateAPIView
+
+
+class SubscriberCreateView(CreateAPIView):
+    queryset = Subscriber.objects.all()
+    serializer_class = SubscriberSerializer
+    permission_classes = (IsAuthenticated, IsAdminUser)
