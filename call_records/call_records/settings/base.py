1ac16f840b134d1b97dc8ad685bc51316607c685
Jéssica Paz <jessicamorim.42@gmail.com>
2018-07-01 11:36:57 -0300

    Setup heroku 2

--- call_records/call_records/settings/base.py
+++ call_records/call_records/settings/base.py
@@ -85,12 +85,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(ROOT_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(ROOT_DIR, 'static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
