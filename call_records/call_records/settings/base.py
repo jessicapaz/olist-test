5d504f5f556e9355719d9f17e6e150b6df4f40d4
Jéssica Paz <jessicamorim.42@gmail.com>
2018-07-01 11:40:43 -0300

    Setup heroku 3

--- call_records/call_records/settings/base.py
+++ call_records/call_records/settings/base.py
@@ -88,11 +88,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(ROOT_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(ROOT_DIR, 'static'),
-)
-
+ 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 AUTH_USER_MODEL = 'users.User'
