12fa6ec4dbf98aa1cfdad700df4fa5f0f0f92761
Jéssica Paz <jessicamorim.42@gmail.com>
2018-07-01 15:14:23 -0300

    Add Subscriber model

--- call_records/api/models.py
+++ call_records/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from validators.phone_validator import validate_phone
+
+class Subscriber(models.Model):
+    phone_number = models.CharField(
+        max_length=11,
+        validators=[validate_phone],
+        unique=True
+    )
+
+    def __str__(self):
+        return self.phone_number
