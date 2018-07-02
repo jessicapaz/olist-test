101accab5f4e051cd46141fcd51b2f03adfd97f0
Jéssica Paz <jessicamorim.42@gmail.com>
2018-07-02 12:19:57 -0300

    Add call record (end and start) model and serializer

--- call_records/api/models.py
+++ call_records/api/models.py
@@ -3,11 +3,39 @@ from django.db import models
 from validators.phone_validator import validate_phone
 
 class Subscriber(models.Model):
-    phone_number = models.CharField(
-        max_length=11,
+    phone_number = models.BigIntegerField(
         validators=[validate_phone],
         unique=True
     )
 
     def __str__(self):
         return self.phone_number
+
+
+class CallStartRecord(models.Model):
+    timestamp = models.DateTimeField(
+        auto_now=False,
+        auto_now_add=False
+    )
+    call_id = models.IntegerField(
+    )
+    source = models.BigIntegerField(
+        validators=[validate_phone]
+    )
+    destination = models.BigIntegerField(
+        validators=[validate_phone]
+    )
+
+    def __str__(self):
+        return f'{self.source} - {self.destination}'
+
+class CallEndRecord(models.Model):
+    timestamp = models.DateTimeField(
+        auto_now=False,
+        auto_now_add=False
+    )
+    call_id = models.IntegerField(
+    )
+
+    def __str__(self):
+        return str(self.call_id)
--- call_records/api/serializers.py
+++ call_records/api/serializers.py
@@ -1,8 +1,22 @@
 from rest_framework.serializers import ModelSerializer
 
 from .models import Subscriber
+from .models import CallStartRecord
+from .models import CallEndRecord
+
 
 class SubscriberSerializer(ModelSerializer):
     class Meta:
         model = Subscriber
+        fields = '__all__'
+
+class CallStartRecordSerializer(ModelSerializer):
+    class Meta:
+        model = CallStartRecord
+        fields = '__all__'
+    
+
+class CallEndRecordSerializer(ModelSerializer):
+    class Meta:
+        model = CallEndRecord
         fields = '__all__'
