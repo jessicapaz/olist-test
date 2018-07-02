a447a3fe4d6b26293fa4fcb96fcfe1297ba843c1
Jéssica Paz <jessicamorim.42@gmail.com>
2018-07-02 17:41:48 -0300

    Rename CallStartRecordTestCase to CallRecordTestCase

--- call_records/api/tests/test_call_record_view.py
+++ call_records/api/tests/test_call_record_view.py
@@ -9,7 +9,7 @@ from rest_framework.test import force_authenticate
 from users.models import User
 
 
-class CallStartRecordTestCase(APITestCase):
+class CallRecordTestCase(APITestCase):
     url = '/call_record/'
 
     def setUp(self):
@@ -22,7 +22,7 @@ class CallStartRecordTestCase(APITestCase):
     
     def test_create_call_start_record(self):
         data = {
-            'id':1,
+            'id': 1,
             'type': 'start',
             'timestamp': '2016-02-29T12:00:00Z',
             'call_id': 70,
@@ -35,7 +35,7 @@ class CallStartRecordTestCase(APITestCase):
     
     def test_create_call_end_record(self):
         data = {
-            'id':1,
+            'id': 1,
             'type': 'end',
             'timestamp': '2016-02-29T12:00:00Z',
             'call_id': 70
