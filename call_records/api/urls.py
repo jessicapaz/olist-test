65f1084d6b0b67f1101e9d5bdd14c2abe92480fa
Jéssica Paz <jessicamorim.42@gmail.com>
2018-07-02 12:22:41 -0300

    Add call_record url

--- call_records/api/urls.py
+++ call_records/api/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from .views import SubscriberCreateView
-
+from .views import CallRecordCreateView
 
 urlpatterns = [
     path(
         'subscriber/', 
         SubscriberCreateView.as_view(), 
         name="subscriber-create"
-    )
+    ),
+    path(
+        'call_record/',
+        CallRecordCreateView.as_view()
+    ),
 ]
