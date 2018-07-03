c81d7e1fd43396155c79815301231df658d6d335
Jéssica Paz <jessicamorim.42@gmail.com>
2018-07-03 11:07:53 -0300

    Fix validate_phone

--- call_records/validators/phone_validator.py
+++ call_records/validators/phone_validator.py
@@ -1,7 +1,10 @@
 from django.core.exceptions import ValidationError
 
+
 def validate_phone(phone):
-    if 10 < len(str(phone)) > 11:
+    phone_length = len(str(phone))
+
+    if phone_length > 11 or phone_length < 10:
         raise ValidationError(
             f'{phone} is not a valid phone number, the size must be 10 or 11.',
             params={'phone': phone},
