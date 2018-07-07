61fe61dd925df710165fd5582423a163c300d71e
Jessica Paz <jessicamorim.42@gmail.com]>
2018-07-07 18:40:08 -0300

    Add bill and create bill record signal

---
+++ call_records/api/bill.py
@@ -0,0 +1,21 @@
+from .models import Price
+
+
+class Bill():
+    def __init__(self, call_start):
+        self.call_start = call_start
+    
+    def _get_tarrif_type(self):
+        hour = self.call_start.timestamp.hour
+        if 6 <= hour < 22:
+            return Price.objects.filter(tarrif_type='standard').last()
+        elif 22 >= hour < 6:
+            return  Price.objects.filter(tarrif_type='reduced').last()
+    
+    def calculate_price(self, duration):
+        minutes = int(duration.total_seconds()/60)
+        tarrif = self._get_tarrif_type()
+        price = float(tarrif.call_charge) * minutes + float(tarrif.standing_charge)
+        return price
+        
+    
---
+++ call_records/api/signals.py
@@ -0,0 +1,33 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from .models import CallEndRecord
+from .models import CallStartRecord
+from .models import Subscriber
+from .models import BillRecord
+
+from .bill import Bill
+
+import datetime
+
+@receiver(post_save, sender=CallEndRecord)
+def create_bill_record(sender, instance, created, **kwargs):
+    if created:
+        call_id = instance.call_id
+        call_start = CallStartRecord.objects.get(call_id=call_id)
+        subscriber = Subscriber.objects.get(id=call_start.source)
+
+        duration = instance.timestamp - call_start.timestamp
+        duration_format = (datetime.datetime.min + duration).time()
+        start_hour = call_start.timestamp.hour
+        bill = Bill(call_start)
+        price = bill.calculate_price(duration)
+
+        BillRecord.objects.create(
+            subscriber=subscriber,
+            call_start_record=call_start,
+            call_duration=duration_format,
+            reference_month=instance.timestamp.month,
+            reference_year=instance.timestamp.year,
+            call_price=price
+        )
