5716dd1c248d79215f9d7d8134ed19342f057cf7
Jéssica Paz <jessicamorim.42@gmail.com>
2018-07-20 10:47:47 -0300

    Add swagger docs

--- call_records/api/urls.py
+++ call_records/api/urls.py
@@ -9,11 +9,19 @@ from .views import BillRecordView
 
 from rest_framework.routers import SimpleRouter
 
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Call Records API')
+
 router = SimpleRouter()
 router.register('subscriber', SubscriberViewSet, base_name='subscriber')
 router.register('price', PriceViewSet, base_name='price')
 
 urlpatterns = [
+    path(
+      '',
+      schema_view  
+    ),
     path(
         'auth/',
         obtain_jwt_token
--- call_records/call_records/settings/base.py
+++ call_records/call_records/settings/base.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
     'users',
 
     'rest_framework',
+    'rest_framework_swagger'
 ]
 
 MIDDLEWARE = [
